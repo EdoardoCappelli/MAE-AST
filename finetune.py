9d8266993c0ca35cf9ec63be54398575490f51e1
Edoardo Cappelli <98023490+EdoardoCappelli@users.noreply.github.com>
2025-06-28 17:33:41 +0200

    Update finetune.py

--- finetune.py
+++ finetune.py
@@ -6,11 +6,10 @@ import os
 import random
 from torch.utils.data import Subset, DataLoader
 import torch.nn as nn 
-from dataloader_finetuning import AudioToSpectrogram, VoxCelebGenderDataset, ESCAudioDataset, collate_fn_spectrogram
 from pathlib import Path 
 from config import Config 
 from mae import MAE 
-from dataloader_finetuning import AudioToSpectrogram, VoxCelebGenderDataset, ESCAudioDataset, collate_fn_spectrogram
+from dataloader.dataloader_finetuning import AudioToSpectrogram, VoxCelebGenderDataset, ESCAudioDataset, collate_fn_spectrogram
 from colorama import Fore, Style, init
 import matplotlib.pyplot as plt
 from torch.optim import lr_scheduler
