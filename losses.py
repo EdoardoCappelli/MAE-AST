1a2808909ead0e5865d7c28f5480ea9a11d33c02
Edoardo Cappelli <98023490+EdoardoCappelli@users.noreply.github.com>
2025-06-28 13:15:12 +0200

    Update losses.py

--- losses.py
+++ losses.py
@@ -1,9 +1,6 @@
 import torch
 import torch.nn.functional as F 
 
-# -------------------------------------
-#               LOSSES
-# -------------------------------------
 def mae_loss(target, recon_logits):
     # masked_target = target[bool_mask]
     # masked_recon = recon_logits[bool_mask]
@@ -11,11 +8,7 @@ def mae_loss(target, recon_logits):
     return loss_recon
 
 def infoNCE_loss(target, class_logits):
-    # masked_class_logits = class_logits[bool_mask]
-    # masked_targets = target[bool_mask]
-    
     all_dots = torch.matmul(target, class_logits.transpose(-1, -2))
     log_softmax = torch.log_softmax(all_dots, dim=-1)
     loss_info_nce = -torch.mean(torch.diagonal(log_softmax, dim1=-2, dim2=-1))
-    
     return loss_info_nce
